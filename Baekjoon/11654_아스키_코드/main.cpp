067fe844de8628e51e08e8b2315ba46bb99bd172
Hyekyoung Kim <hyekyoung@postech.ac.kr>
2020-09-29 15:21:37 +0900

    Problem #11654 Complete

---
+++ Baekjoon/11654_아스키_코드/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+/* Problem #11654 ASCII Code */
+/* https://www.acmicpc.net/problem/11654 */
+int main() {
+	char c;
+	cin >> c;
+	cout << static_cast<int>(c);
+	return 0;
+}
