6b2ca10002f468ea97ab39bb97f088a45d2a0a6f
Hyekyoung Kim <hyekyoung@postech.ac.kr>
2020-09-30 13:43:47 +0900

    Problem #2839 Complete

---
+++ Baekjoon/2839_설탕_배달/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/* Problem #2839 ŠEĆER */
+/* https://www.acmicpc.net/problem/2839 */
+int main() {
+	int count_five = 0, count_three = 0, N;
+	cin >> N;
+	while (N > 0 && N % 5 != 0) {
+		N -= 3;
+		if (N < 0)
+			break;
+		count_three++;
+	}
+	if (N < 0)
+		cout << -1;
+	else {
+		count_five = N / 5;
+		cout << count_three + count_five;
+	}
+
+	return 0;
+}
