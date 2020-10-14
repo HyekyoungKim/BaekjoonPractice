d17d227b6a5a0bb8a4a6fcefd88bbded58437a73
Hyekyoung Kim <hyekyoung@postech.ac.kr>
2020-10-14 13:04:04 +0900

    Problem #1978 Complete

---
+++ Baekjoon/Math2/1978_소수_찾기/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/* Problem #1978 소수 찾기 */
+/* https://www.acmicpc.net/problem/1978 */
+int main() {
+	int N, count = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int k;
+		cin >> k;
+		bool prime = true;
+		if (k == 1)
+			prime = false;
+		else {
+			for (int j = 2; j <= k / 2; j++)
+				if (k % j == 0) {
+					prime = false;
+					break;
+				}
+		}
+		if (prime)
+			count++;
+	}
+	cout << count;
+	return 0;
+}
