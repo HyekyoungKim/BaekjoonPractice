9bbb51c6d261490f999e87f4ac4dd3163cf2a8b8
Hyekyoung Kim <hyekyoung@postech.ac.kr>
2020-10-14 13:20:57 +0900

    Problem #2581 Complete

---
+++ Baekjoon/Math2/2581_소수/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+/* Problem #2581 ¼Ò¼ö(Prime Numbers) */
+/* https://www.acmicpc.net/problem/2581 */
+int main() {
+	int M, N, sum = 0, min; // All prime numbers are greater than 1.
+	cin >> M >> N;
+	min = N + 1;
+	for (int i = M; i <= N; i++) {
+		bool prime = true;
+		if (i == 1)
+			prime = false;
+		else {
+			for (int k = 2; k <= i / 2; k++)
+				if (i % k == 0) {
+					prime = false;
+					break;
+				}
+		}
+		if (prime) {
+			sum += i;
+			min = (i < min ? i : min);
+		}
+	}
+	if (sum > 0) {
+		cout << sum << endl;
+		cout << min << endl;
+	}
+	else
+		cout << -1 << endl;
+	return 0;
+}
